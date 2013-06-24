d747ae7ba32323dc23fb655fb78280be23e16f16
Davy-jones-Giant-squid <wasabine@gmail.com>
2013-06-24 15:50:44 -0500

    models

--- scrape_crime/management/commands/retrieve_data.py
+++ scrape_crime/management/commands/retrieve_data.py
@@ -1,18 +1,27 @@
+from django.core.management.base import BaseCommand
+
 import urllib2
 import json
-
-from django.core.management.base import BaseCommand
+import logging
 
 from scrape_crime.models import Crime
 
+
+log = logging.getLogger('main')
+#This will read the data from the url and hold them in memory
+url = urllib2.urlopen('http://data.cityofchicago.org/resource/ijzp-q8t2.json').read()
+#this will deserialize the data and return a dictionary
+data = json.loads(url)
+
 class Command(BaseCommand):
-	def handle(self):
-		#This will read the data from the url and hold them in memory
-		url = urllib2.urlopen('http://data.cityofchicago.org/resource/ijzp-q8t2.json').read()
-		#this will deserialize the data and return a dictionary
-		data = json.loads(url)
+	help = 'Retrieve the data from cityofchicago.org'
+
+	def handle(self, *args, **kwargs):
+		log.debug('Retrieving data from cityofchicago.org')
 
 		for datum in data:
+			log.debug("Checking %s" % datum)
+
 			non_unicode_keys = dict((str(k), v) for k, v in datum.iteritems())
 			location_non_unicode = dict((str(k), v) for k, v in non_unicode_keys['location'].iteritems())
 			obj = Crime(**non_unicode_keys)
--- scrape_crime/models.py
+++ scrape_crime/models.py
@@ -4,26 +4,21 @@ class Crime(models.Model):
 	"""
 	Model that represents the Chicago crime data API
 	"""
-	ID = models.IntegerField(null=True, blank=True)
-	case_number = models.CharField(max_length=8, default='')
-	x_coordinate = models.IntegerField(null=True, blank=True)
-	y_coordinate = models.IntegerField(null=True, blank=True)
-	description = models.CharField(max_length=30, null=False)
+	ID = models.IntegerField()
+	case_number = models.CharField(max_length=8)
+	description = models.CharField(max_length=30)
 	domestic = models.BooleanField()
 	date = models.DateTimeField()
-	year = models.IntegerField(null=True, blank=True)
 	arrest = models.BooleanField()
-	district = models.CharField(max_length=5, null=False)
+	district = models.CharField(max_length=5)
 	location = models.ForeignKey('Location')
-	location_description = models.CharField(max_length=30, null=False)
-	community_area = models.IntegerField(null=True, blank=True)
-	primary_type = models.CharField(max_length=10, null=False)
-	beat = models.IntegerField(null=True, blank=True)
-	ward = models.IntegerField(null=True, blank=True)
-	iucr = models.CharField(max_length=5, null=False)	
+	location_description = models.CharField(max_length=30)
+	primary_type = models.CharField(max_length=10)
+	beat = models.IntegerField(blank=True)
+	ward = models.IntegerField( blank=True)
+	iucr = models.CharField(max_length=5)	
 	updated_on = models.DateTimeField()
-	fbi_code = models.CharField(max_length=3, null=False)
-	block = models.CharField(max_length=30, null=False)
+	fbi_code = models.CharField(max_length=3)
 
 
 	def __unicode__(self):
@@ -34,3 +29,5 @@ class Location(models.Model):
 	latitude = models.DecimalField(max_digits=20, decimal_places=14)
 	longitude = models.DecimalField(max_digits=20, decimal_places=14)
 	needs_recoding = models.BooleanField()
+	community_area = models.IntegerField(blank=True)
+	block = models.CharField(max_length=30 )
