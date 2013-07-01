a9f07324848ab35061e6ea355362308bfdaed75d
Davy-jones-Giant-squid <wasabine@gmail.com>
2013-07-01 17:10:16 -0500

    created admin

--- chicagocrime/settings.py
+++ chicagocrime/settings.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     'scrape_crime',
     'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
---
+++ scrape_crime/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from scrape_crime.models import Crime
+
+admin.site.register(Crime)
