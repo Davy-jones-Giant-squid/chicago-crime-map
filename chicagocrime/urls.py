44a145f561bc51b73daebd1cb80895590e74a5a4
Davy-jones-Giant-squid <wasabine@gmail.com>
2013-07-02 09:53:28 -0500

    Updated templates

--- chicagocrime/urls.py
+++ chicagocrime/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'chicagocrime.views.home', name='home'),
+    url(r'^$', 'scrape_crime.views.map'),
     # url(r'^chicagocrime/', include('chicagocrime.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- scrape_crime/views.py
+++ scrape_crime/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from scrape_crime.models import Crime, Location
+
+def map(request):
+	coordinates = Location.objects.order_by('id')
+	output = ["It happened on: %s.\n" %l.block for l in coordinates]
+	return HttpResponse(output)
