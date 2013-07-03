547a537491405851a43bf12f44a04513556a0425
Davy-jones-Giant-squid <wasabine@gmail.com>
2013-07-03 14:06:15 -0500

    pinpoint location + info window

--- scrape_crime/views.py
+++ scrape_crime/views.py
@@ -14,15 +14,23 @@ def map(request):
 	gmap = maps.Map(opts = {
 		'center': myLatlng,
 		'mapTypeId': maps.MapTypeId.ROADMAP,
-		'zoom': 11,
+		'zoom': 13,
 		'mapTpeControlOptions':{
 			'style': maps.MapTypeControlStyle.DROPDOWN_MENU
 		},
 		})
 
-	marker = maps.Marker({
-		'position': myLatlng,
-		'map' : gmap,
-		})
+	obj = Crime.objects.order_by('id')
+	for i in obj[:10]:
+		marker = maps.Marker(opts = {
+			'position': maps.LatLng(i.location.latitude, i.location.longitude),
+			'map' : gmap,
+			})
+		maps.event.addListener(marker, 'mouseover','myobj.markerOver')
+		maps.event.addListener(marker, 'mouseout', 'myobj.markerOut')
+		info = maps.InfoWindow({
+			'content': i.primary_type,
+			})
+		info.open(gmap, marker)
 	context = {'form': MapForm(initial={'map':gmap})}
 	return render_to_response('map.html', context)
